83f04e9aaf88ac931176ebf671ee104cf4c96a4b
andersonsilva7 <andersonsilva7.ti@gmail.com>
2023-02-12 17:44:38 -0300

    relembrando git

---
+++ 06/js/scripts.js
@@ -0,0 +1,34 @@
+var nome = "Anderson";
+var sobrenome = "Silva";
+
+var nomecompleto  = nome +" "+ sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+
+console.log(typeof 'asv');
+
+console.log(nomecompleto);
+
+console.log(typeof '5.2323');
+
+var frase = "Esta é uma frase complexa";
+
+console.log(frase);
+
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Ele disse  "Olá"');
+
+document.write("Ele disse  'Olá'");
+
+
+document.write('Ele disse  "Olá"');
+
+console.log("este numero:" + '432');
+
+
+console.log("este numero" + nome);
