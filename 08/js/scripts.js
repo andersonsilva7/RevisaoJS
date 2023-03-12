1d2e8f4e43082ec12c50a87800e4fd5887ac956c
andersonsilva7 <andersonsilva7.ti@gmail.com>
2023-03-12 19:18:57 -0300

    .

---
+++ 08/js/scripts.js
@@ -0,0 +1,15 @@
+// Hoisting - içamento
+console.log(sobrenome);
+console.log(numero);
+
+var nome = null;
+var sobrenome = "Silva";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Anderson";
+
+console.log(nome);
+
+var numero = 5;
