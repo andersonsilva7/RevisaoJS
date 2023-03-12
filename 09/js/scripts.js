f6102d95a6d5e6f9d53f8a88564f232b91266bca
andersonsilva7 <andersonsilva7.ti@gmail.com>
2023-03-12 19:47:41 -0300

    .

---
+++ 09/js/scripts.js
@@ -0,0 +1,29 @@
+var obj = {
+    nome: 'Anderson',
+    idade: 30,
+    profissao: 'Dev',
+    estartrabalhando: true,
+};
+
+console.log(obj);
+
+
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.profissao);
+
+console.log(" O meu nome é " + obj.nome);
+
+obj.nome = "Alan";
+
+console.log(obj.nome);
+
+console.log(obj);
+
+obj.graduacao = true;
+
+console.log(obj);
+
+
+
+
