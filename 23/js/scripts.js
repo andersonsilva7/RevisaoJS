d45b7519ca36a00cd45c6a92494877ee5e351e74
andersonsilva7 <andersonsilva7.ti@gmail.com>
2023-07-31 07:32:16 -0300

    Comandos basicos

---
+++ 23/js/scripts.js
@@ -0,0 +1,22 @@
+alert('Olá mudo')
+confirm('Você digitou errado')
+var palavra = window.prompt('Digite a palavra correta:')
+
+alert('A palavra correta é ' + palavra)
+
+var n1 = Number(window.prompt('Digite um número:- '))
+var n2 = Number(window.prompt('Digite outro número: '))
+var soma = n1 + n2
+
+window.alert(`a soma entre ${n1} e ${n2} é igual a ${soma}`)
+
+//parseFloat
+//toString
+
+var nome = 'Anderson'
+
+var idade = 30
+
+var nota = 7.5
+
+console.log(`O Aluno ${nome} com ${idade} anos tirou a nota ${nota}`)
