370300adabc719bfc08a126e556af3c3b72465fc
andersonsilva7 <andersonsilva7.ti@gmail.com>
2023-08-01 19:04:30 -0300

    Comandos basicos

--- 23/js/scripts.js
+++ 23/js/scripts.js
@@ -4,14 +4,16 @@ var palavra = window.prompt('Digite a palavra correta:')
 
 alert('A palavra correta é ' + palavra)
 
-var n1 = Number(window.prompt('Digite um número:- '))
+var n1 = Number(window.prompt('Digite um número: '))
 var n2 = Number(window.prompt('Digite outro número: '))
 var soma = n1 + n2
 
 window.alert(`a soma entre ${n1} e ${n2} é igual a ${soma}`)
 
-//parseFloat
-//toString
+//.parseFloat(n)
+//.parseInt(n)
+//String(n)
+//n.toString()
 
 var nome = 'Anderson'
 
